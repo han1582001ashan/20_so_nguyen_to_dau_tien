bdae2faeac76b7f8b65eabb2a8fba21b4bcc75c1
han1582001ashan <83277320+han1582001ashan@users.noreply.github.com>
2024-08-19 19:09:03 +0700

    nopbai

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace DisplayHello
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int number = 20;
+            int count = 0;
+            int n = 2;
+            while (count < number)
+            {
+                int i = 2;
+                bool check = true;
+                while (i <= Math.Sqrt(n))
+                {
+                    if (n % i == 0)
+                    {
+                        check = false;
+                        break;
+
+                    }
+                    i++;
+                    
+                }
+                if (check)
+                {
+                    Console.WriteLine(n + " is a prime");
+                    count++;
+                }
+                n++;
+            }
+        }
+    }
+}
+
